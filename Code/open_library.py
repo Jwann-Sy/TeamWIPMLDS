53236e07618db0bd82e39ab6476f29f9e2718b90
SuperCastro <77307701+SuperCastro@users.noreply.github.com>
2023-04-27 13:14:40 -0700

    Add files via upload

---
+++ Code/open_library.py
@@ -0,0 +1,9 @@
+import os
+
+# =========== OPEN A DIRECTORY =======================
+# Opens a directory, just type in the directory path. 
+def send_to_dir():
+    os.startfile(r'C:\Users\spagh\Desktop\CS 532 Project\Sounds')
+
+# Main
+send_to_dir()
